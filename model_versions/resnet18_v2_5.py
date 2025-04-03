7b6e1c218d1d72867a7db4bd89392a871d9e8f97
Stefano Bertolani <stefano.bertolani@symboolic.com>
2025-04-03 10:26:52 +0200

    Migration to v3.6
    - Replace Sequence Decoder with Simple Attention
    
    - 2 Blocks depth
    
    - Resnet with an extra resnet layer and an extra maxpool
    
    - Moved v2.5 inside model_versions folder

--- model_versions/resnet18_v2_5.py
+++ model_versions/resnet18_v2_5.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 
 def conv3x3(in_planes, out_planes, stride=1):
+
     return nn.Conv2d(in_planes, out_planes, kernel_size=3,
                      stride=stride, padding=1, bias=False)
 
@@ -40,18 +41,18 @@ class BasicBlock(nn.Module):
 
 class ResNet18(nn.Module):
 
-    def __init__(self, nb_feat=384):
+    def __init__(self, nb_feat = 384):
+        
         self.inplanes = nb_feat // 4
         super(ResNet18, self).__init__()
         self.conv1 = nn.Conv2d(1, nb_feat // 4, kernel_size=3, stride=2, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(nb_feat // 4, eps=1e-05)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(BasicBlock, nb_feat // 8, 2, stride=1)
-        self.layer2 = self._make_layer(BasicBlock, nb_feat // 4, 2, stride=1)
-        self.layer3 = self._make_layer(BasicBlock, nb_feat // 2, 2, stride=1)
-        self.layer4 = self._make_layer(BasicBlock, nb_feat, 2, stride=1)
-        
+        self.layer1 = self._make_layer(BasicBlock, nb_feat // 4, 2, stride=1)
+        self.layer2 = self._make_layer(BasicBlock, nb_feat // 2, 2, stride=2)
+        self.layer3 = self._make_layer(BasicBlock, nb_feat, 2, stride=1)
+
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
@@ -77,9 +78,11 @@ class ResNet18(nn.Module):
 
         x = self.layer1(x)
         x = self.layer2(x)
-        x = self.maxpool(x)
         x = self.layer3(x)
-        x = self.layer4(x)
         x = self.maxpool(x)
         
         return x
+
+
+
+
